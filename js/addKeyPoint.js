9000919c1dc36c3994eef1d64f47d58edc197338
Vicken <vicken.liu@gmail.com>
2016-04-05 22:55:06 +1200

    the stage point showing

---
+++ js/addKeyPoint.js
@@ -0,0 +1,21 @@
+
+
+
+function addKeyPoint(){
+	var div  = creatediv('div','keyPoint','+')
+
+	var note = creatediv('div','keyPoinNote noteUp','key point')
+	
+	div.appendChild(note);
+	return div;
+}
+
+
+function creatediv(tag,className,text){
+	var element = document.createElement(tag);
+	element.className += className;
+	element.innerHTML = text;
+	return element;
+}
+
+module.exports=addKeyPoint;
--- js/index.js
+++ js/index.js
@@ -1,1 +1,22 @@
-var hello ='no way'
+var addKeyPoint= require('./addKeyPoint');
+var $ = require('jquery');
+require('jquery-ui/draggable');
+require('jquery-ui/sortable');
+
+
+$(document).ready(function(){
+	$('.addBtn').click(function(){
+		$(this).before(addKeyPoint());
+		moveKeyPoint();
+	})
+
+	// initial the keyPoints are draggable
+	moveKeyPoint();
+
+});
+
+	
+function moveKeyPoint(){
+	$('.keyPoint').draggable({ axis: "x" });
+	$('.keyPoinNote').draggable({ axis: "y" });
+}
