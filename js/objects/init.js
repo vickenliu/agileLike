3d8f3972febd26f707880697ab556eb607eec531
Vicken <vicken.liu@gmail.com>
2016-04-20 17:36:28 +1200

    update kp to using socket

--- js/objects/init.js
+++ js/objects/init.js
@@ -14,18 +14,20 @@ const keypoint=new Keypoint
 module.exports = () => {
 	$('.addBtn').dblclick(function(){
 		$('#keyPointDiv').append( pointTemplate({tagid: Date.now().toString(), left: '87%', notePosition:'-50px', noteContent:'new point'}) );
-		postData('/keypoints', keypoint.getKeyPointInfo($('.keyPoint').last()) )
+    var info= keypoint.getKeyPointInfo($('.keyPoint').last())
+    postData('/keypoints', info )
 		moveKeyPoint();
+    socket.emit('KPchange', info)
 	})
 	// initial the keyPoints are draggable
     loadKeyPoints()
     moveKeyPoint();
-    socketEvent(pointTemplate,moveKeyPoint)
+    socketEvent(moveKeyPoint)
     $('#newpost').on('click',helper.alertFn)
 }
 
 
-function socketEvent(pointTemplate,moveKeyPoint){
+function socketEvent(moveKeyPoint){
 	    socket.on('change',function(msg){
           var ele= $(".post[data-id='"+msg.tagid+"']");
           if($(ele).length>0){
@@ -41,9 +43,16 @@ function socketEvent(pointTemplate,moveKeyPoint){
           }
         })
         socket.on('KPchange',function(data){
-          $(".keyPoint[data-id='"+data.tagid+"']").css({
+          var ele= $(".keyPoint[data-id='"+data.tagid+"']")
+          if($(ele).length>0){
+            $(ele).css({
               'left':data.left
             }).find('.keyPoinNote').css('top',data.notePosition).find('.nameInfo').text(data.noteContent)
+          }else{
+            $('#keyPointDiv').prepend( pointTemplate(data))
+            moveKeyPoint()
+          }
+
         })
         socket.on('delete',function(id){
         	 $(".post[data-id='"+id+"']").remove()
